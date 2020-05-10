98de5a3e79501c1d00c82cadaac9a60d09327a77
subhashkchand <62471424+subhashkchand@users.noreply.github.com>
2020-05-10 14:44:04 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask,render_template,url_for,request
 import pandas as pd 
+import numpy as np
 
 import pickle
 
@@ -7,16 +8,33 @@ import pickle
 loaded_model=pickle.load(open('random_forest_regression_model.pkl', 'rb'))
 app = Flask(__name__)
 
+# @app.route('/')
+# def home():
+# 	return render_template('home.html')
+
 @app.route('/')
 def home():
-	return render_template('home.html')
+	return render_template('Randomforest.html')
+
+# @app.route('/predict',methods=['POST'])
+# def predict():
+#     df=pd.read_csv('real_2018.csv')
+#     my_prediction=loaded_model.predict(df.iloc[:,:-1].values)
+#     my_prediction=my_prediction.tolist()
+#     return render_template('result.html',prediction = my_prediction)
 
 @app.route('/predict',methods=['POST'])
 def predict():
-    df=pd.read_csv('real_2018.csv')
-    my_prediction=loaded_model.predict(df.iloc[:,:-1].values)
-    my_prediction=my_prediction.tolist()
-    return render_template('result.html',prediction = my_prediction)
+    input_features = [int(i) for i in request.form.values()]
+    features = [np.array(input_features)]
+    aqi_prediction = loaded_model.predict(features)
+    
+    return render_template('Randomforest.html', prediction_text = 'Predicted AQI is:{}'.format(aqi_prediction))
+    
+#     df=pd.read_csv('real_2018.csv')
+#     my_prediction=loaded_model.predict(df.iloc[:,:-1].values)
+#     my_prediction=my_prediction.tolist()
+#     return render_template('result.html',prediction = my_prediction)
 
 
 if __name__ == '__main__':
